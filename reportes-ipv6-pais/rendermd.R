98d724a21b7b968e52cf13af3d6209acb2436721
Carlos M. Martinez <carlos@lacnic.net>
2017-08-16 14:52:30 -0300

    v2 del reporte, actualizada, parametrizada

--- reportes-ipv6-pais/rendermd.R
+++ reportes-ipv6-pais/rendermd.R
@@ -1,2 +1,10 @@
 library(rmarkdown)
-rmarkdown::render('ipv6uptake-parametrized-v2.Rmd')
+
+args <- commandArgs(trailingOnly=T)
+print(args)
+theCC <- args[2]
+theCName <- args[3]
+theRDate <- "20170814"
+
+
+rmarkdown::render('ipv6uptake-parametrized-v2.Rmd', params=list(CC=theCC, CName=theCName, RDate=theRDate) )
