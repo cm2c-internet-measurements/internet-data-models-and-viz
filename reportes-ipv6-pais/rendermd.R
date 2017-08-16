33fe79f73dd54d8cae746d6bf764e6fe48d3fe30
Carlos Martinez <carlos@lacnic.net>
2017-08-16 14:17:57 -0400

    Render with params

--- reportes-ipv6-pais/rendermd.R
+++ reportes-ipv6-pais/rendermd.R
@@ -5,6 +5,7 @@ print(args)
 theCC <- args[2]
 theCName <- args[3]
 theRDate <- "20170814"
+theOutput <- "html_document"
 
 
-rmarkdown::render('ipv6uptake-parametrized-v2.Rmd', params=list(CC=theCC, CName=theCName, RDate=theRDate) )
+rmarkdown::render('ipv6uptake-parametrized-v2.Rmd', params=list(CC=theCC, CName=theCName, RDate=theRDate, output=theOutput) )
