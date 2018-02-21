322e23f0c99ff9a62aba97b83b8caf513d8b375e
Carlos M. Martinez <carlos@lacnic.net>
2018-02-21 18:02:36 -0300

    added R presentation for phase 3 Model

--- lacnic-ipv4-fase3-model/freeipv4_model_f3.R
+++ lacnic-ipv4-fase3-model/freeipv4_model_f3.R
@@ -24,33 +24,37 @@ tsFromMongoDate <- function(x) {
 
 # Getting and cleaning data
 # setwd("~/scratch")
-d <- fromJSON("http://opendata.labs.lacnic.net/ipv4stats/ipv4avail/lacnic?lastdays=320")
-# d <- fromJSON("freeipv4_lacnic.json")
-e <- unlist(d[1], use.names=FALSE, recursive=FALSE)
-f <- t(data.frame(e))
 
-dates_free4 = lapply(f[,1], tsFromMongoDate)
-free4 = lapply(f[,2], as.integer)
+getCleanIPv4Data <- function() {
+  ## TODO: get lastday offset automatically, not in a fixed way
+  d <- fromJSON("http://opendata.labs.lacnic.net/ipv4stats/ipv4avail/lacnic?lastdays=320")
+  # d <- fromJSON("freeipv4_lacnic.json")
+  e <- unlist(d[1], use.names=FALSE, recursive=FALSE)
+  f <- t(data.frame(e))
+  
+  dates_free4 = lapply(f[,1], tsFromMongoDate)
+  free4 = lapply(f[,2], as.integer)
+  
+  free4 = t ( as.matrix( as.data.frame(free4)))
+  free4 = as.numeric(free4[,1])
+  free4 = free4 + 908800 ## TODO: fetch reserved ipv4 automatically
+  
+  dates_free4 = t ( as.matrix( as.data.frame(dates_free4)))
+  dates_free4 = as.numeric(dates_free4[,1])
+  
+  ipv4 = NULL
+  ipv4 = cbind(ipv4, dates_free4)
+  ipv4 = cbind(ipv4, free4)
+  ipv4 = as.data.frame(ipv4)
+  
+  return(ipv4)
+}
 
-free4 = t ( as.matrix( as.data.frame(free4)))
-free4 = as.numeric(free4[,1])
-free4 = free4 + 908800
-
-dates_free4 = t ( as.matrix( as.data.frame(dates_free4)))
-dates_free4 = as.numeric(dates_free4[,1])
-# model = lsfit(dates_free4, free4)
-
-ipv4 = NULL
-ipv4 = cbind(ipv4, dates_free4)
-ipv4 = cbind(ipv4, free4)
-ipv4 = as.data.frame(ipv4)
+ipv4 <- getCleanIPv4Data()
 
 ## Modeling and prediction
-# model4 = lm(free4 ~ dates_free4)
-# ipv4$free4 = ipv4$free4 + 908800
 # m5 = lm(free4 ~ dates_free4, data = ipv4)
 m5 = lm(free4 ~ poly(dates_free4, 1), data = ipv4)
-# m5 = auto.arima(ts(ipv4$free4))
 pr2 = predict(m5, interval="confidence")
 
 # plot(dates_free4, free4)
@@ -58,8 +62,8 @@ pr2 = predict(m5, interval="confidence")
 
 # Forecasting ----------------------------------------------------
 secsday = 86400
-future_horizon_days = 1000
-future_dates_free4 = seq(dates_free4[1], dates_free4[1]+secsday*future_horizon_days, secsday)
+future_horizon_days = 365*3
+future_dates_free4 = seq(ipv4$dates_free4[1], ipv4$dates_free4[1]+secsday*future_horizon_days, secsday)
 future_free4 = predict(m5, newdata = data.frame(dates_free4=future_dates_free4))
 fcast = as.data.frame( cbind(future_dates_free4, future_free4))
 cutoff = which(fcast$future_free4 < 0)
@@ -69,17 +73,18 @@ cutoff_date = as.Date(as.POSIXct(cutoff_ts, origin="1970-01-01"))
 # -----------------------------------------------------------------
 
 # Plotting
-plot(c(future_dates_free4, dates_free4), c(future_free4, pr2[,'fit']), col='green', lwd=1, xlab='', ylab='', xaxt="n", yaxt="n")
+plot(c(future_dates_free4, ipv4$dates_free4), c(future_free4, pr2[,'fit']), 
+     col='green', lwd=1, xlab='', ylab='', xaxt="n", yaxt="n")
 axis(side=2, seq(from=0, to=5)*1e6, c("0", "1M", "2M", "3M", "4M", "5M"))
 ttlabels = c("2017-1-1","2018-1-1", "2019-1-1", "2020-1-1", "2021-1-1")
 tticks = as.numeric(as.POSIXct(ttlabels, tz="UTC"))
 axis(side=1, tticks, ttlabels)
 title("LACNIC IPv4 Exhaustion Phase 3 Model", sub="Linear model", xlab="Time", ylab="Free IPv4 Addresses (millions)")
-lines(dates_free4, free4, col='black', lwd=3, xlab='')
-lines(c(future_dates_free4, dates_free4), numeric(length(c(future_dates_free4, dates_free4))), col='yellow', xlab='')
+lines(ipv4$dates_free4, ipv4$free4, col='black', lwd=3, xlab='')
+lines(c(future_dates_free4, ipv4$dates_free4), numeric(length(c(future_dates_free4, ipv4$dates_free4))), col='yellow', xlab='')
 points(future_dates_free4[cutoff_offset], 0, col='red', lwd=5, xlab='')
 text(cutoff_ts, 300000, labels=c(cutoff_date))
 
-print(summary(m5))
+#print(summary(m5))
 
 ## end
